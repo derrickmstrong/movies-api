f2bec681bfff7ca86b289e9c1adc1d307de60c78
Derrick Strong <derrickmstrong@gmail.com>
2024-02-20 09:57:54 -0600

    Initial commit

---
+++ src/main/java/com/derrickstrong/movies/ReviewRepository.java
@@ -0,0 +1,9 @@
+package com.derrickstrong.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
